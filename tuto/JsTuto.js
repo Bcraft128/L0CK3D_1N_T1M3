9b4cd0df6348d0da1f0728d85f8450a3a4af7363
Bcraft128 <45488899+Bcraft128@users.noreply.github.com>
2019-11-05 18:22:54 +0100

    Update JsTuto.js

--- tuto/JsTuto.js
+++ tuto/JsTuto.js
@@ -1,1 +1,6 @@
-
+function testtuto1() {
+  if (document.getElementById("inptuto1").value == "")
+  { document.location.href="/test1"; }
+  else
+  { document.getElementById("demo").innerHTML = nope;}
+}
